a783c31510b3f17f1fe5b0f70a02f553fee65d6e
alesamsil <alesamsil@gmail.com>
2017-07-21 11:26:13 -0300

    teste ale

--- app/src/main/java/com/example/luisf/finance/MainActivity.java
+++ app/src/main/java/com/example/luisf/finance/MainActivity.java
@@ -8,7 +8,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //Para Teste
+        //Para OTAREIO Ale
         setContentView(R.layout.activity_main);
     }
 }
