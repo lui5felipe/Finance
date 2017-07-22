f7ed359ccde143e6bd3400e445bcd77f8c4db249
alesamsil <alesamsil@gmail.com>
2017-07-22 03:22:09 -0300

    Merge branch 'master' of C:\Users\yuri\Desktop\Hack\Finance with conflicts.

--- app/src/main/java/com/example/luisf/finance/telaLogin.java
+++ app/src/main/java/com/example/luisf/finance/telaLogin.java
@@ -3,11 +3,11 @@ package com.example.luisf.finance;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class telaLogin extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_tela_login);
     }
 }
