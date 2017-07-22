c8ce90225d3ada214c7cbddf71f47c95b5b4aa03
1320587 <willian_oliveira@id.uff.br>
2017-07-22 01:35:58 -0300

    AAA

--- app/src/main/java/com/example/luisf/finance/MainActivity.java
+++ app/src/main/java/com/example/luisf/finance/MainActivity.java
@@ -2,12 +2,23 @@ package com.example.luisf.finance;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+    public void onCreate2(View view) {
+        setContentView(R.layout.activity_main2);
+    }
+
+    public void onCreate3(View view) {
         setContentView(R.layout.activity_main);
     }
 }
