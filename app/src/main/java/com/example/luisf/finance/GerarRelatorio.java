41e17d6f35509b793d69d09dcfd40f7eb099e89c
1320587 <willian_oliveira@id.uff.br>
2017-07-22 10:26:43 -0300

    Merge branch 'master' of C:\Users\hp\Desktop\Projeto\Finance with conflicts.

--- app/src/main/java/com/example/luisf/finance/GerarRelatorio.java
+++ app/src/main/java/com/example/luisf/finance/GerarRelatorio.java
@@ -5,16 +5,22 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 
 public class GerarRelatorio extends AppCompatActivity {
 
+    boolean show;
+    ImageButton llConteudo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        show = true;
         setContentView(R.layout.gerarrelatorio);
+        llConteudo = (ImageButton) findViewById(R.id.imageButton);
     }
 
-    public void startSecondActivity(View view) {
+   /* public void startSecondActivity(View view) {
         Intent secondActivity = new Intent(this, Main2Activity.class);
             EditText editText = (EditText)findViewById(R.id.editText);
             String texto = editText.getText().toString();
@@ -22,5 +28,18 @@ public class GerarRelatorio extends AppCompatActivity {
             infos.putString("editText", texto);
             secondActivity.putExtras(infos);
         startActivity(secondActivity);
-    }
+    }*/
+   public void teste ()
+   {
+       if(show)
+       {
+           llConteudo.-f[99];
+           show = false;
+       }else {
+           llConteudo.setVisibility(View.VISIBLE);
+           show = true;
+       }
+
+   }
+
 }
