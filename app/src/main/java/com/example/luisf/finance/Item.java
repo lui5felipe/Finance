fe445962c255bdbc41846f1d23209a141c731fdd
Thaynara <thaynara.al.cardoso@gmail.com>
2017-07-22 19:01:51 -0300

    Tela de Buscar Itens

---
+++ app/src/main/java/com/example/luisf/finance/Item.java
@@ -0,0 +1,63 @@
+package com.example.luisf.finance;
+
+/**
+ * Created by Thaynara on 22/07/2017.
+ */
+
+public class Item {
+    String nomeItem = new String();
+    String codItem = new String();
+    double precoItem;
+    int qtdItem;
+
+
+    public Item (String nomeItem, String codItem, double precoItem, int qtd){
+        this.setNomeItem(nomeItem);
+        this.setCodItem(codItem);
+        this.setPrecoItem(precoItem);
+        this.setQtdItem(qtd);
+    }
+
+    public String getNomeItem() {
+        return nomeItem;
+    }
+
+    public void setNomeItem(String nomeItem) {
+        this.nomeItem = nomeItem;
+    }
+
+    public String getCodItem() {
+        return codItem;
+    }
+
+    public void setCodItem(String codItem) {
+        this.codItem = codItem;
+    }
+
+    public double getPrecoItem() {
+        return precoItem;
+    }
+
+    public void setPrecoItem(double precoItem) {
+        this.precoItem = precoItem;
+    }
+
+    public int getQtdItem() {
+        return qtdItem;
+    }
+
+    public void setQtdItem(int qtd) {
+        this.qtdItem = qtd;
+    }
+
+    public void inicializa(){
+
+        this.nomeItem = "Barra de Cereal";
+        this.codItem = "123";
+        this.precoItem = 1.20;
+
+
+
+    }
+}
+
