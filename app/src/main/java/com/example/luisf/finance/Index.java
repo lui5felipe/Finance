edfffb72f2649f69b651531191efbbd55047dbad
1320587 <willian_oliveira@id.uff.br>
2017-07-22 03:20:15 -0300

    bla bla

--- app/src/main/java/com/example/luisf/finance/Index.java
+++ app/src/main/java/com/example/luisf/finance/Index.java
@@ -5,18 +5,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class Index extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.Index);
 
     }
 
     public void startSecondActivity(View view) {
 
-        Intent secondActivity = new Intent(this, Main2Activity.class);
+        Intent secondActivity = new Intent(this, GerarRelatorio.class);
         startActivity(secondActivity);
     }
 }
