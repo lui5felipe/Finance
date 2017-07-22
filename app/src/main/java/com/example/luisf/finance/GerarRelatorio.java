de2e14c9e482bc8e5ae9d0aa4d8bd0643d58fdd0
1320587 <willian_oliveira@id.uff.br>
2017-07-22 11:39:09 -0300

    kakakakaka99999

--- app/src/main/java/com/example/luisf/finance/GerarRelatorio.java
+++ app/src/main/java/com/example/luisf/finance/GerarRelatorio.java
@@ -10,29 +10,20 @@ import android.widget.ImageView;
 
 public class GerarRelatorio extends AppCompatActivity {
 
-    Button bbuscar = (Button)findViewById(R.id.buscaRelatorio);
-    ImageView grafico = (ImageView)findViewById(R.id.grafRelatorio);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.gerarrelatorio);
+        setContentView(R.layout.activity_gerar_relatorio);
+        Button bbuscar = (Button)findViewById(R.id.buscaRelatorio);
+        final ImageView grafico = (ImageView)findViewById(R.id.grafRelatorio);
 
-        bbuscar.setOnClickListener(new View.OnClickListener() {
+       bbuscar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 grafico.setVisibility(View.VISIBLE);
             }
         });
     }
-
-    /*public void startSecondActivity(View view) {
-        Intent secondActivity = new Intent(this, Main2Activity.class);
-            EditText editText = (EditText)findViewById(R.id.editText);
-            String texto = editText.getText().toString();
-            Bundle infos = new Bundle();
-            infos.putString("editText", texto);
-            secondActivity.putExtras(infos);
-        startActivity(secondActivity);
-    }*/
 }
