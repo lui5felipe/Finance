71be8a0ee64dcca2a55b2cde36e5108e169186a0
alesamsil <alesamsil@gmail.com>
2017-07-23 00:36:28 -0300

    gerenciar itens 02 03

---
+++ app/src/main/java/com/example/luisf/finance/gerenciaritens03.java
@@ -0,0 +1,13 @@
+package com.example.luisf.finance;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class gerenciaritens03 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gerenciaritens03);
+    }
+}
